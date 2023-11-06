7f0d75756ef249661c8e8170822f13ec83306ef0
Protick Roy <protickrr@gmail.com>
2023-11-06 12:44:48 +0600

    debugging 502 api/values/all #1

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -19,10 +19,14 @@ class Fib extends Component {
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('/api/values/all');
-    this.setState({
-      seenIndexes: seenIndexes.data,
-    });
+    try{
+      const seenIndexes = await axios.get('/api/values/all');
+      this.setState({
+        seenIndexes: seenIndexes.data,
+      });
+    }catch (err){
+      console.log(err.message);
+    }
   }
 
   handleSubmit = async (event) => {
